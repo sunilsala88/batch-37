438ab50bac11d804f8083026c813f0ba3eb54384
sunilsala88 <sunilsala88@gmail.com>
2026-04-11 17:06:44 +0530

    apr11-7

--- 3_datastructure/data1.py
+++ 3_datastructure/data1.py
@@ -18,3 +18,24 @@ print(type(l1))
 print(l1[0])
 print(l1[-1])
 print(l1[1:3])
+
+#add element in list
+l1.append(88)
+print(l1)
+
+l1.insert(2,99)
+print(l1)
+
+#remove element from list
+l1.remove(33)
+print(l1)
+
+l1.pop(2)
+print(l1)
+
+del l1[-1]
+print(l1)
+
+#update element in list
+l1[1]=100
+print(l1)
---
+++ 3_datastructure/data2.py
@@ -0,0 +1,24 @@
+
+stock_price={'tsla':100,'apple':200,'google':300,'meta':400}
+#key:int,float,string,bool
+#value:any data type
+
+#access
+print(stock_price['tsla'])
+print(stock_price.get('apple'))
+
+#add
+stock_price['amazon']=500
+stock_price.update({'microsoft':600})
+print(stock_price)
+
+
+#update
+stock_price['tsla']=150 
+stock_price.update({'google':400})
+print(stock_price)
+
+#delete
+del stock_price['meta']
+stock_price.pop('google')
+print(stock_price)
