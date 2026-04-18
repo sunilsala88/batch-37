c880950cd8e3fdebf1ee6ed6da95bd1fce88a98d
sunilsala88 <sunilsala88@gmail.com>
2026-04-18 17:14:25 +0530

    apr-18-2

--- 5_loops/loop1.py
+++ 5_loops/loop1.py
@@ -1,6 +1,7 @@
+#iteration,looping
 prices=[33,44,55,66,77]
 
-
+#type 1 loop
 total=0
 for p in prices:
     # print(p)
@@ -9,3 +10,18 @@ for p in prices:
 print(total)
 
 print('average price is', total/len(prices))
+
+new_list=[]
+
+for p in prices:
+    new_list.append(p**2)
+
+print(new_list)
+
+#type 2 loop
+print('hello world')
+
+print(list(range(100)))
+
+for i in range(10):
+    print('hello world',i)
