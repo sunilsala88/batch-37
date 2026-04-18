c0c06e707b1c3abdd79973e03d5513d93b5483c0
sunilsala88 <sunilsala88@gmail.com>
2026-04-18 17:41:08 +0530

    apr-18-66

--- 5_loops/loop2.py
+++ 5_loops/loop2.py
@@ -11,3 +11,13 @@ for i,j in stock_prices.items():
     print(i,j)
 
 list1=[4,6,9,1,5]
+m=list1[0]
+for i in list1:
+    if i>m:
+        m=i
+
+print(m)
+
+
+v=[200,300,400,500,600]
+p=[20,30,40,50,60]
