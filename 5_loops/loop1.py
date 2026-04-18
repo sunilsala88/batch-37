745f1a64e0171b0bca0e9e606c69e99e3adee093
sunilsala88 <sunilsala88@gmail.com>
2026-04-18 17:23:59 +0530

    apr18-5

--- 5_loops/loop1.py
+++ 5_loops/loop1.py
@@ -1,5 +1,5 @@
 #iteration,looping
-prices=[33,44,55,66,77]
+prices=[33,44,55,66,77,66]
 
 #type 1 loop
 total=0
@@ -25,3 +25,10 @@ print(list(range(100)))
 
 for i in range(10):
     print('hello world',i)
+
+
+#type 3 loop
+new_list=[]
+for i in range(len(prices)):
+    new_list.append(prices[i]**2)
+print(new_list)
