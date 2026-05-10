86c0057b0a3aabe194a52701c01be261412a142d
sunilsala88 <sunilsala88@gmail.com>
2026-05-10 17:13:39 +0530

    may10-7

--- 5_loops/while1.py
+++ 5_loops/while1.py
@@ -11,19 +11,34 @@
 # [5,10,15...50...95 100]
 
 
-multipler=[]
-count=20
-number=5
+# multipler=[]
+# count=20
+# number=5
 
-#loop should stop when we have 10 numbers in the multipler list
-while True:
-    if len(multipler)==count:
-        break
+# #loop should stop when we have 10 numbers in the multipler list
+# while True:
+#     if len(multipler)==count:
+#         break
 
-    multipler.append(number)
-    number=number+5    
+#     multipler.append(number)
+#     number=number+5    
 
-print(multipler)
+# print(multipler)
 
 #list of 10 fib numbers
 #[0,1,1,2,3,5,8,13,21,34]
+
+fib=[0,1]
+count=10
+while True:
+    if len(fib)==count:
+        break
+    prev=fib[-1]
+    prev_prev=fib[-2]
+    current=prev+prev_prev
+    fib.append(current)
+print(fib)
+
+
+lisst1=[34,25,68,21,11,45,32]
+#find the largest number in the list
