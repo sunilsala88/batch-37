535be45de0f74f83f6e242f12c1635c96a1322ac
sunilsala88 <sunilsala88@gmail.com>
2026-05-10 16:57:08 +0530

    may10-3

--- 5_loops/while1.py
+++ 5_loops/while1.py
@@ -1,8 +1,29 @@
 
 
-number= 1
+# number= 1
+# while True:
+#     if number ==10:
+#         break
+#     print(number)
+#     number=number+1
+
+#n=20
+# [5,10,15...50...95 100]
+
+
+multipler=[]
+count=20
+number=5
+
+#loop should stop when we have 10 numbers in the multipler list
 while True:
-    if number ==10:
+    if len(multipler)==count:
         break
-    print(number)
-    number=number+1
+
+    multipler.append(number)
+    number=number+5    
+
+print(multipler)
+
+#list of 10 fib numbers
+#[0,1,1,2,3,5,8,13,21,34]
