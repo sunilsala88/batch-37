46b751bafa24e28475dbae3ae8c7aad6b952a4b9
sunilsala88 <sunilsala88@gmail.com>
2026-05-17 16:53:49 +0530

    may17-3

--- 6_function/fun1.py
+++ 6_function/fun1.py
@@ -32,14 +32,21 @@ print(result)
 
 
 
+def get_fibonacci(n:int)->list:
+    fib=[0,1]
+    last=1
+    second_last=0
+    for i in range(n-2):
+        current=last+second_last
+        fib.append(current)
+        second_last=last
+        last=current
+        
+    return (fib)
 
-fib=[0,1]
-last=1
-second_last=0
-for i in range(8):
-    current=last+second_last
-    fib.append(current)
-    second_last=last
-    last=current
-    
-print(fib)
+
+fib_numbers=get_fibonacci(20)
+print(fib_numbers)
+
+#function to get the largest number in the list
+#create a rev function which takes a list as input and returns the reversed list as output
