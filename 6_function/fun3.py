59a5b43240c8e651c9d94194d9accaf6c9af8a36
sunilsala88 <sunilsala88@gmail.com>
2026-05-17 18:26:30 +0530

    may1700

--- 6_function/fun3.py
+++ 6_function/fun3.py
@@ -31,3 +31,32 @@ age=35
 l1=[1,2,3,4,5]
 print('hello my name is '+name+' and my age is '+str(age))
 print(f"hello my name is {name} and my age is {l1}")
+
+
+
+#4. Variable-Length Arguments (*args and **kwargs)
+
+# ➤ *args → Multiple positional arguments
+
+def add_numbers(*args):
+    print(args)
+    return sum(args)
+
+print(add_numbers(1, 2, 3, 4,5))
+
+
+def print_details(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_details(name="Anil", age=30, country="India", email="some@gmail.com")
+
+
+#lamda
+def square(x):
+    return x**2
+s=square(5)
+print(s)
+
+square = lambda x: x**2
+print(square(5))
