ef1f7efb6a7fdf2d42377b3eb1d80baad4c25da4
sunilsala88 <sunilsala88@gmail.com>
2026-05-17 17:20:30 +0530

    may17-22

--- 6_function/fun2.py
+++ 6_function/fun2.py
@@ -6,15 +6,18 @@ a='hello world'
 def average(list1:list)->float:
 
     #local variable
-    print(a)
+    print(price)
     total=0
     for i in list1:
         total=total+i
     avg=total/len(list1)
     return avg
 
+
 prices=[100,200,300,400,500]
 avg_price=average(prices)
 print(avg_price)
 print(total)
 print(a)
+price=440
+
