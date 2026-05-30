61ecb1043ab7d8a1a67a68fb16dc0f13b4d627e3
sunilsala88 <sunilsala88@gmail.com>
2026-05-30 17:11:38 +0530

    may30-55

--- 7_file_exception/file1.py
+++ 7_file_exception/file1.py
@@ -6,7 +6,12 @@
 # f1.close()
 
 
-d1=open('/Users/algo trading 2026/batch 37/7_file_exception/demo1.txt','r')
-d=d1.read()
-print(d)
-d1.close()
+# d1=open('/Users/algo trading 2026/batch 37/7_file_exception/demo1.txt','r')
+# d=d1.read()
+# print(d)
+# d1.close()
+
+
+a1=open('demo3.txt','a')
+a1.write('\nthis is my 4th line')
+a1.close()
