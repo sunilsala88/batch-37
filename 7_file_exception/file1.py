e74a060e3fdd601f49a07670ab213efb700ac1c2
sunilsala88 <sunilsala88@gmail.com>
2026-05-30 17:05:21 +0530

    may30-2

--- 7_file_exception/file1.py
+++ 7_file_exception/file1.py
@@ -1,6 +1,12 @@
 #text,csv,exel,json,pickle
 
-data='this is my 2nd line'
-f1=open('file1.txt','w')
-f1.write(data)
-f1.close()
+# data='this is my 2nd line'
+# f1=open('file1.txt','w')
+# f1.write(data)
+# f1.close()
+
+
+d1=open('/Users/algo trading 2026/batch 37/7_file_exception/demo1.txt','r')
+d=d1.read()
+print(d)
+d1.close()
