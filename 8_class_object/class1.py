2e6bc8aefb1da3b4ea523a445a45821627a673b1
sunilsala88 <sunilsala88@gmail.com>
2026-05-31 17:04:20 +0530

    may31-2

--- 8_class_object/class1.py
+++ 8_class_object/class1.py
@@ -1,8 +1,9 @@
 #class-> class is a blueprint of an object
 #object-> object is an instance of a class
 #pass-> pass is a placeholder for future code. It is used when we want to create a class or a function but we don't want to write the code for it yet.
-
-
+#attribute-> attribute is a variable that is associated with an object. It is used to store data about the object.
+#class attribute-> class attribute is a variable that is associated with a class. It is shared by all instances of the class.
+#object attribute-> object attribute is a variable that is associated with an object. It is unique to each instance of the class.
 def demo(x,y):
     return x+y
 
@@ -15,9 +16,15 @@ print('some random line')
 
 
 class Student:
-    pass
+    school_name='ABC School' #class attribute
+    dress_code='Uniform' #class attribute
 
-s1=Student()
+    def __init__(self,n,a,e):
+        self.name=n #object attribute
+        self.age=a #object attribute
+        self.email=e #object attribute
+
+s1=Student('John',20,'john@example.com')
 print(s1)
-s2=Student()
+s2=Student('Jane',22,'jane@example.com')
 print(s2)
