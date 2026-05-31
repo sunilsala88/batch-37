4c59fb2a5851ac5c2304d0701fceb9c8d7f2a667
sunilsala88 <sunilsala88@gmail.com>
2026-05-31 17:15:56 +0530

    may31-66

--- 8_class_object/class1.py
+++ 8_class_object/class1.py
@@ -4,6 +4,9 @@
 #attribute-> attribute is a variable that is associated with an object. It is used to store data about the object.
 #class attribute-> class attribute is a variable that is associated with a class. It is shared by all instances of the class.
 #object attribute-> object attribute is a variable that is associated with an object. It is unique to each instance of the class.
+#method-> method is a function that is associated with an object. It is used to perform some action on the object.
+#__init__-> __init__ is a special method that is called when an object is created. It is used to initialize the object.
+#it is called contructor in other programming languages.
 def demo(x,y):
     return x+y
 
@@ -24,7 +27,13 @@ class Student:
         self.age=a #object attribute
         self.email=e #object attribute
 
+    def introduce(self):
+        intro= f'My name is {self.name} and I am {self.age} years old. My email is {self.email} and I study in {self.school_name}.'
+        return intro
+
 s1=Student('John',20,'john@example.com')
 print(s1)
+print(s1.introduce())
 s2=Student('Jane',22,'jane@example.com')
 print(s2)
+print(s2.introduce())
