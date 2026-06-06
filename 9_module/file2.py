c0b8c0ce38d55473097c9a0ac546c1c1e94c84eb
sunilsala88 <sunilsala88@gmail.com>
2026-06-06 17:00:42 +0530

    sat-june6-2

--- 9_module/file2.py
+++ 9_module/file2.py
@@ -1,6 +1,12 @@
 
+#never name your file same as built in module name
+#pypi.org
 import file1
-
+import random
+import time
+import math
+import os
+import sys
 print(file1.a)
 file1.abc()
 
@@ -8,3 +14,10 @@ file1.abc()
 c1=file1.Circle(5)
 print(c1.area())
 print(c1.circumference())
+
+print(random.random())
+time.sleep(1)
+print(random.randint(1,10))
+print(math.sqrt(16))
+print(os.getcwd())
+sys.exit()
