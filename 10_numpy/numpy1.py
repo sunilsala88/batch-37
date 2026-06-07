115f3b96a15954f479a8b02f8ba22c15e43beb83
sunilsala88 <sunilsala88@gmail.com>
2026-06-07 17:04:08 +0530

    june-7-2

--- 10_numpy/numpy1.py
+++ 10_numpy/numpy1.py
@@ -1,5 +1,19 @@
 import numpy as np
-l1=[1,2,3,4,5]
-print(l1)
-np1=np.array(l1)
-print(np1)
+# l1=[1,2,3,4,5]
+# np1=np.array(l1)
+# print(np1)
+
+# l2=[6,7,8,9,10]
+# np2=np.array(l2)
+# print(np2)
+# print(l1+l2)
+# print(np1/np2)
+
+print(list(range(10)))
+np3=np.arange(10,50,2)
+print(np3)
+
+print(np.ones(5,dtype=int))
+print(np.zeros(5))
+
+#multidimensional array
