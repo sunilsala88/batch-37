a1e7931ec6c85a650c1b5fb580af79a82f5c1b22
sunilsala88 <sunilsala88@gmail.com>
2026-06-27 17:28:02 +0530

    jun-27-2

--- datetime_analy/datetime1.py
+++ datetime_analy/datetime1.py
@@ -20,3 +20,14 @@ print(t1)
 
 delta1=dt.timedelta(minutes=5)
 print(d1+delta1)
+
+thursdays=[]
+first_date=dt.datetime(2025,1,1)
+
+for i in range(365):
+    if first_date.weekday()==3:
+        thursdays.append(first_date)
+    first_date=first_date+dt.timedelta(days=1)
+    print(first_date)
+
+print(thursdays)
