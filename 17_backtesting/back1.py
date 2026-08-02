77c86c0eb3d986e4eb5357eff9e71d679c473eb2
sunilsala88 <sunilsala88@gmail.com>
2026-08-02 17:43:59 +0530

    aug-2-5

--- 17_backtesting/back1.py
+++ 17_backtesting/back1.py
@@ -6,10 +6,10 @@ print(data)
 
 import pandas_ta as ta
 
-def sma(closing_price,period):
+def calculate_sma(closing_price,period):
     return ta.sma(closing_price,length=period)
 
-def ema(closing_price,period):
+def calculate_ema(closing_price,period):
     return ta.ema(closing_price,length=period)
 
 from backtesting import Backtest, Strategy
@@ -20,17 +20,20 @@ class SMAcrossover(Strategy):
     e1=20
 
     def init(self):
-        pass
+        closing_price=self.data.Close.s
+        self.sma=self.I(calculate_sma,closing_price,self.s1)
+        self.ema=self.I(calculate_ema,closing_price,self.e1)
 
     def next(self):
         pass
 
 
-sma=sma(data['Close'],50)
-ema=ema(data['Close'],20)
+sma=calculate_sma(data['Close'],50)
+ema=calculate_ema(data['Close'],20)
 print(sma)
 print(ema)
 
-# bt=Backtest(data, SMAcrossover,cash=10_00_000)
-# result=bt.run()
-# bt.plot()
+bt=Backtest(data, SMAcrossover,cash=10_00_000)
+result=bt.run()
+print(result)
+bt.plot()
